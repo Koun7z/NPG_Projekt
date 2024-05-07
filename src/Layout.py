d217b1b78776318df6dc904a52d75584cbb0434c
n2oneProgrammer <dominikwojtasik191@gmail.com>
2024-05-07 18:32:27 +0200

    Skeleton UIManager

--- src/Layout.py
+++ src/Layout.py
@@ -4,7 +4,7 @@ import pygame
 
 
 class Layout:
-    def render(self, window: pygame.window, events: List[pygame.event.Event]):
+    def render(self, window: pygame.display, events: List[pygame.event.Event]):
         raise NotImplementedError
 
     def close(self):
