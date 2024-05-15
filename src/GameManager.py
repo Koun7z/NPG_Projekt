5167f4c2eb1102108195af97f734cd9a03a7ea8e
Piotr Woliński <pwolinski03@gmail.com>
2024-05-15 14:32:09 +0200

    Dodano opcje usuwania tekstu Dodano geter listy wpisanych znaków

--- src/GameManager.py
+++ src/GameManager.py
@@ -1,4 +1,5 @@
 from typing import List
+
 import pygame
 
 
@@ -18,11 +19,20 @@ class GameManager:
         self._player_input = []
         self.CHAR_LIST = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
                           't', 'u', 'v', 'w', 'x', 'y', 'z', 'ą', 'ę', 'ó', 'ź', 'ż', ',', '.', '?', ':', ';', '1', '2',
-                          '3', '4', '5', '6', '7', '8', '9', '0']
+                          '3', '4', '5', '6', '7', '8', '9', '0', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-',
+                          '_', '+', '=', '`', '~']
 
     def handle_input(self, event: pygame.event.Event):
 
         if event.type != pygame.KEYDOWN:
+            return
+
+        if event.key == pygame.K_BACKSPACE:
+            self._player_input.pop()
+            return
+
+        if event.key == pygame.K_ESCAPE:
+            self.clear_input()
             return
 
         if event.key == pygame.K_SPACE:
@@ -32,3 +42,9 @@ class GameManager:
         for char in event.unicode:
             if char.lower() in self.CHAR_LIST:
                 self._player_input.append(char)
+
+    def get_input(self):
+        return self._player_input
+
+    def clear_input(self):
+        self._player_input.clear()
