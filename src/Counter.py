32050a0c665bc2b967d07636523658019fed7b77
n2oneProgrammer <dominikwojtasik191@gmail.com>
2024-05-16 09:15:11 +0200

    Implementation Start and stop Counter

--- src/Counter.py
+++ src/Counter.py
@@ -1,21 +1,32 @@
 from pygame_gui.elements import UILabel
+import time
 
 
 class Counter:
-    _start_time: int
+    _start_time: float
+    _stop_time: float
     _is_active: bool
     _ui_label: UILabel
 
     def __init__(self, *args, **kwargs):
         self._start_time = 0
+        self._stop_time = 0
         self._is_active = False
         self._ui_label = UILabel(*args, **kwargs)
 
     def start(self) -> bool:
-        raise NotImplementedError
+        if self._is_active:
+            return False
+        self._is_active = True
+        self._start_time = time.time()
+        return True
 
     def stop(self) -> bool:
-        raise NotImplementedError
+        if not self._is_active:
+            return False
+        self._is_active = False
+        self._stop_time = time.time()
+        return True
 
     def reset(self):
         raise NotImplementedError
