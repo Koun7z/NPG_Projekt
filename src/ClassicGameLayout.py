4e34e3a10e08a33c88fbb2e97bed04d50931207d
Piotr Woliński <pwolinski03@gmail.com>
2024-05-17 17:56:19 +0200

    Small correction after recent merge

--- src/ClassicGameLayout.py
+++ src/ClassicGameLayout.py
@@ -49,7 +49,7 @@ class ClassicGameLayout(Layout):
 
         text_surface = ui_manager.render_input_text_surface()
         # TODO: Offset calculation should be done somewhere else preferably only once and updated if window size changes
-        left_offset = (ui_manager.get_width_window() - self.get_font_of("target_text").size(game_manager.get_target_text)[0]) / 2
+        left_offset = (ui_manager.get_width_window() - self.get_font_of("target_text").size(game_manager.get_target_text())[0]) / 2
         top_offset = ui_manager.get_height_window() / 2
         window.blit(text_surface, (left_offset, top_offset))
 
