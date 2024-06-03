6da4634717dceb4fdaaf830dabb1bf24d37fd72e
Piotr Woliński <pwolinski03@gmail.com>
2024-06-03 18:02:24 +0200

    Added medium difficulty text

--- test/game_manager_test.py
+++ test/game_manager_test.py
@@ -3,13 +3,27 @@ import src.GameManager
 from src.Enumerators import Mode, Difficulty
 
 
-def test_load_by_char():
+def test_load_by_char_hard():
 
     game_manager = src.GameManager.GameManager()
 
     game_manager._current_mode = Mode.Classic           # \
                                                         # |-> Those parameters dictate checked data set
     game_manager._current_difficulty = Difficulty.Hard  # /
+
+    for i in range(10000):
+        for j in range(100, 1001, 50):
+            game_manager.load_target_list_n_chars(j)
+            assert len("".join(game_manager._target_text)) == j
+
+
+def test_load_by_char_medium():
+
+    game_manager = src.GameManager.GameManager()
+
+    game_manager._current_mode = Mode.Classic             # \
+                                                          # |-> Those parameters dictate checked data set
+    game_manager._current_difficulty = Difficulty.Medium  # /
 
     for i in range(10000):
         for j in range(100, 1001, 50):
